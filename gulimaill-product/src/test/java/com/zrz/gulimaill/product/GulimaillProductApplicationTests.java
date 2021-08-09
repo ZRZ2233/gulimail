56fc88decaa294cbf18c27b02090f4b41d7c9e2d
ZRZ2233 <67986349+ZRZ2233@users.noreply.github.com>
2021-08-09 18:07:06 +0800

    项目初始结构

---
+++ gulimaill-product/src/test/java/com/zrz/gulimaill/product/GulimaillProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrz.gulimaill.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimaillProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
